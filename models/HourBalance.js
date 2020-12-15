282c94f571d75dbf4d7a0613b6f82f99a6d5b7c9
akmalsalem <akmalsalem@gmail.com>
2020-12-15 20:24:55 +0200

    akmalmodels2

--- models/HourBalance.js
+++ models/HourBalance.js
@@ -2,14 +2,14 @@ const mongoose=require("mongoose");
 
 const HourBalance=mongoose.Schema({
     id:{
-        type:string,
+        type:String,
         required:true,
         unique:true,
         minLength:[4,"Id is too short"]
         //at least we have 2 characters(ac,hr or 43 for example), a dash(-) and at least one more number
     },
     hours:{
-        type:number,
+        type:Number,
         default:0
     }
 })
