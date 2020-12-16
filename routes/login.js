b66150cafa3b00724b8de0d45247a55fff42cd94
jamesst78 <mohadsamir@hotmail.com>
2020-12-16 22:10:52 +0200

    7amada

---
+++ routes/login.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const route = express.Router();
+const bcryptjs=require("bcryptjs");
+const staffMember=require("../models/staffMember");
+
+route.post("/",(req,res)=>{
+    
+})
