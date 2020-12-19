a00f65b1e2e0d66c5f38f084edfb64409cc0df61
Reem Salah <reeem.saalah20@gmail.com>
2020-12-19 12:04:58 +0200

    change day off

--- routes/academic.js
+++ routes/academic.js
@@ -5,6 +5,7 @@ const repreq=require("./academicsRoutes/replacementrequest")
 const slotlinking=require("./academicsRoutes/slotlinking");
 const viewre=require("./academicsRoutes/viewallrequests");
 const viewslots=require("./academicsRoutes/viewunassignedslots");
+const changedayoff=require("./academicsRoutes/changedayoff");
 
 //routes
 route.use("/viewschedule",viewSchedule);
@@ -12,6 +13,7 @@ route.use("/replacementrequest",repreq);
 route.use("/slotlinking",slotlinking);
 route.use("/viewallrequests",viewre);
 route.use("/viewunassignedslots",viewslots);
+route.use("/changedayoff",changedayoff);
 
 
 
