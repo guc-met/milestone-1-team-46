63b8549ccc9c9994ba64892d1031cc2fb50ba36e
Reem Salah <reeem.saalah20@gmail.com>
2020-12-22 20:51:23 +0200

    a member can not view other courses' assignment

--- routes/academicsRoutes/viewunassignedslots.js
+++ routes/academicsRoutes/viewunassignedslots.js
@@ -10,13 +10,19 @@ route.get("/", async(req, res)=>{
     try{
         const id=req.id;
         const member= await staffMember.findOne({id:id});
+        const myCourses= member.courses;
+        const course=req.body.course;
         if(! member){
             return res.status(400).json({msg:"incorrect credentials"});        
         }
        if(!member.ac){
             return res.status(401).json({msg:"unauthorized"});            
        }
-    const course=req.body.course;
+       if(!myCourses.includes(course))
+       {
+        return res.status(401).json({msg:"unauthorized"});            
+       }
+    
     const result= await teachingSlots.find({"slot.course":course});
     res.send(result);
 
